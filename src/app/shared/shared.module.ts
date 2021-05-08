76a984c598e5328651df3c12c6977ec2513ec110
Bret Williams <straightener@hotmail.com>
2021-05-08 16:23:40 -0500

    refactored to modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,20 @@
 //ng g m shared/shared --flat -m products/product.module
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { StarComponent } from './star.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    StarComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    StarComponent
   ]
 })
 export class SharedModule { }
