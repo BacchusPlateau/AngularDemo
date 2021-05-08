34397d90c3517532ef446211feeca2184b72e5ca
Bret Williams <straightener@hotmail.com>
2021-05-08 16:22:03 -0500

    refactored modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+//ng g m shared/shared --flat -m products/product.module
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
